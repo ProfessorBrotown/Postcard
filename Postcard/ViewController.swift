09ff9f18278cb483340e24e8dfa764da54086551
Macbook Pro <Admin@Macbooks-MacBook-Pro.local>
2014-10-26 14:03:06 -0700

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+
+    @IBOutlet weak var mailButton: UIButton!
+        
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false
+        //makes the Label button appear after button pressed
+        
+        messageLabel.text = enterMessageTextField.text
+        //makes the message appear near stamp
+        
+        messageLabel.textColor = UIColor.redColor()
+        //text color
+        
+        enterMessageTextField.text = ""
+        //makes the enterMessageTextField go back to blank
+        
+        enterMessageTextField.resignFirstResponder()
+        //makes the keyboard go away!
+        
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        //changes the text of the mail button to show mail has been sent after pressed
+    }
 
 }
 
